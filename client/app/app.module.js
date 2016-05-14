79f8142079218b48ee37176c0e35f50926492101
Summer Wollin <summer.wollin@gmail.com>
2016-05-14 12:29:19 -0600

    align items in projects page

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -13,22 +13,19 @@
   $stateProvider
       .state('home', {
         url: '/',
-        views: {
-            'header': {
-              template: '<sw-header/></sw-header>',
-              controller: 'HeaderController'
-            },
-            'about': {
-              template: '<sw-about></sw-about>'
-            }
-        }
+        templateUrl: 'home.html'
+      })
+      .state('projects', {
+        url: '/projects',
+        templateUrl: 'projects.html'
       });
-      // .state('404', {
-      //   url: '/404',
-      //   views: {
-      //       'body': {
-      //           template: 'Aww snap! 404. You must be lost. <a ui-sref="home">Go home!</a>'
-      //       }
-      //   }
-      });
+
+    });
 }());
+// .state('404', {
+//   url: '/404',
+//   views: {
+//       'body': {
+//           template: 'Aww snap! 404. You must be lost. <a ui-sref="home">Go home!</a>'
+//       }
+//   }
